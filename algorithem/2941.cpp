f75c09c936ce74779a416da31dc0aa9825f0a4e1
rising-jun <jun36817@naver.com>
2022-08-02 15:23:39 +0900

    2941주석삭제

--- algorithem/2941.cpp
+++ algorithem/2941.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 using namespace std;
 
@@ -23,25 +22,3 @@ int main(){
     cout << s.length();
     return 0;
 }
-
-
-// int main(){
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-//     cout.tie(NULL);
-//     int answer = 0;
-//     string s = "";
-//     cin >> s;
-//     string alpha[8] = {"c=","c-","dz=","d-","lj","nj","s=","z="};
-
-//     for(int i = 0; i < s.size(); i++) {
-//         answer++;
-//         for(int j = 0; j < sizeof(alpha)/sizeof(string); j++) {
-//             if(alpha[j].at(0) == s[i] && alpha[j].at(1) == s[i + 1]) {
-//                 i++;
-//             }
-//         }
-//     }
-//     cout << answer;
-//     return 0;
-// }
